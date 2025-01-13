764289bc263b4db68a5d19d11fe31c7fcb376556
rej <rzioma@acm.org>
2025-01-13 22:02:44 +0100

    Pad tensors when converting from PTH to NPZ

--- src/pth_to_npz.py
+++ src/pth_to_npz.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 import numpy as np
 import sys
 import os
@@ -22,14 +23,28 @@ if isinstance(checkpoint, dict):
     layers = [checkpoint[f"layers.{layer_idx}.w"] for layer_idx in range(len(checkpoint))]
 
     assert len(connections) == 2
-
     print("Number of layers: ", len(layers))
 
-    gate_types = [torch.argmax(layer, dim=0) for layer in layers];
-    gate_types = torch.stack(gate_types, dim=0).cpu().numpy()
+    gate_types = [torch.argmax(layer, dim=0) for layer in layers]
+
+    original_size = np.sum([g.size() for g in gate_types] + [c.size() for c in connections[0]] + [c.size() for c in connections[1]])
+
+    def pad_tensor_array(tensors, padding=0):
+        max_size = max(t.size(0) for t in tensors)
+        padded = [F.pad(t, (0, max_size - t.size(0)), value=padding) for t in tensors]
+        return torch.stack(padded, dim=0)
+
+    gate_types = pad_tensor_array(gate_types).cpu().numpy()
+    connections[0] = pad_tensor_array(connections[0]).cpu().numpy()
+    connections[1] = pad_tensor_array(connections[1]).cpu().numpy()
+
+    padded_size = gate_types.size + connections[0].size + connections[1].size 
+    if padded_size > 0:
+        print("Number of new null gates & connections added for padding:", padded_size - original_size)
+
     numpy_dict = {  "gate_types" : gate_types,
                     "connections.A" : connections[0],
                     "connections.B" : connections[1] }
     np.savez(npz_file_name, **numpy_dict)
 
-    print("Number of gates: ", np.size(gate_types))
+    print("Totoal number of gates in the network:", np.size(gate_types))
