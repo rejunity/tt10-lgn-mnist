f2800206658a134d416595df39bf5bfd2e06f2c0
rej <rzioma@acm.org>
2025-09-18 08:48:52 +0200

    F2550x8 96.86% bin_test_acc, lr=0.03, epochs=100, crop 22x22 -> 16x16 inputs.

--- test/test.py
+++ test/test.py
@@ -87,7 +87,8 @@ X = \
 # Y =   "../src/20250915-070516_binTestAcc9760_seed230646_epochs100_2x4000_b256_lr30_interconnect.npz"
 # Y =   "../src/20250916-211123_binTestAcc9600_seed742253_epochs100_8x2550_b256_lr30_interconnect.npz"
 # Y =   "../src/20250917-070356_binTestAcc9698_seed302376_epochs70_8x2550_b256_lr30_interconnect.npz"
-Y =   "../src/20250918-061801_binTestAcc9698_seed492503_epochs70_8x2550_b256_lr30_interconnect.npz"
+# Y =   "../src/20250918-061801_binTestAcc9698_seed492503_epochs70_8x2550_b256_lr30_interconnect.npz"
+Y =     "../src/20250917-215426_binTestAcc9686_seed900305_epochs100_8x2550_b256_lr30_interconnect.npz"
 
 
 ############################## TEST NETS
